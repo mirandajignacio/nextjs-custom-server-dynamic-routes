5e6c6d8e735cf5e366bd576edb5d2a25639325ae
Miranda J. Ignacio <mirandajignacio@gmail.com>
2020-07-01 15:21:17 -0300

    chore: initial commmit

---
+++ pages/[uid].js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div>
+      <h1>Hello Dynamic Route 👋</h1>
+    </div>
+  );
+};
---
+++ pages/product/[uid].js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div>
+      <h1>Hello Dynamic Product 👋</h1>
+    </div>
+  );
+};
